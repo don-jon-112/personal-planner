12dde1464bcd2eb5bb192c9a9eee94f47508ed15
don-jon-112 <exzone45@gmail.com>
2026-07-27 00:32:39 +0700

    Fixing cn

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Panel, PanelHeader, PanelTitle, PanelDescription, PanelContent } from "@/components/ui/panel";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { 
