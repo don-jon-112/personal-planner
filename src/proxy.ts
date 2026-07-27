671bcae965ebfbff7c2d40682a0624c89112aa68
Jonathan <jonathan.alva@ai.astra.co.id>
2026-07-27 14:21:25 +0700

    Fixing

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // If the user is trying to access the login page, let them
   if (request.nextUrl.pathname === '/login') {
     return NextResponse.next()
